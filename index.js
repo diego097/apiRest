59ca791ba1f5551a4a19fcb8351db37c15f317f6
Diego <diego.vega097@gmail.com>
2019-07-25 01:38:58 -0500

    cargando archivos

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const parser = require('body-parser');
+const services = require('./services')
+const handlify = require('./handlers');
+
+
+
+
+
+const usersHandler = handlify('users');
+const postsHandler = handlify('posts');
+
+
+
+const app = express();
+
+
+
+
+
+// parse application
+app.use(parser.urlencoded({ extended: false}));
+
+
+//parse aplication/json
+app.use(parser.json());
+
+
+const port = 3000;
+
+app.get('/', usersHandler(services).get);
+app.post('/', usersHandler(services).post);
+app.put('/:id', usersHandler(services).put);
+app.delete('/:id', usersHandler(services).delete);
+
+
+
+app.get('/posts', postsHandler(services).get);
+app.post('/posts', postsHandler(services).post);
+app.put('/posts:id', postsHandler(services).put);
+app.delete('/posts:id', postsHandler(services).delete);
+
+
+
+app.listen(port, () => console.log('Servidor en el puerto 3000')); 
